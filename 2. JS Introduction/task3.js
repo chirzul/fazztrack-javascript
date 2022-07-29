1bf2e186574a0b0a4da3f4b2da94d44621f8dae5
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-07-29 20:08:26 +0700

    Revisi logika cetak baris

--- 2. JS Introduction/task3.js
+++ 2. JS Introduction/task3.js
@@ -9,7 +9,7 @@ if (typeof printSegitiga == "number" && printSegitiga > 0) {
   for (let i = printSegitiga; i >= 1; i--) {
     let row = "";
     for (let j = 1; j <= i; j++) {
-      row = `${row} ${j}`;
+      row += `${j} `;
     }
     console.log(row);
   }
