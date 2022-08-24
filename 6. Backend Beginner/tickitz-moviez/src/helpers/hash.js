a19fdf7eb5b37121cc50fddaf7d41c0a93649e8a
Muhammad Chirzul Maula <69786243+chirzul@users.noreply.github.com>
2022-08-24 16:23:30 +0700

    Delete 6. Backend Beginner directory

--- 6. Backend Beginner/tickitz-moviez/src/helpers/hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt')
-
-const securePassword = async (password) => {
-  const salt = await bcrypt.genSalt(10)
-  const result = await bcrypt.hash(password, salt)
-  return result
-}
-module.exports = securePassword
--- 6. Backend Beginner/tickitz-moviez/src/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const ctrl = require('../controllers/auth')
-
-router.post('/', ctrl.login)
-
-module.exports = router
